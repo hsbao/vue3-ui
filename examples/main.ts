6d6ac3b0c6f8fb0da14a6ba66ff609cdd6daf303
hongshibao <arvin.hong@corp.to8to.com>
2022-03-07 08:55:17 +0800

    update

--- examples/main.ts
+++ examples/main.ts
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-//  import VUI from '../lib/index.esm.js' // 全局引入
-import Button from '../lib/button' // 按需引入
-import Icon from '../lib/icon'
+ import VUI from '../lib/index.esm.js' // 全局引入
+// import Button from '../lib/button' // 按需引入
+// import Icon from '../lib/icon'
 import 'theme-chalk/src/index.scss'
 
 import App from './App.vue'
 
-createApp(App).use(Button).use(Icon).mount('#app')
+createApp(App).use(VUI).mount('#app')
